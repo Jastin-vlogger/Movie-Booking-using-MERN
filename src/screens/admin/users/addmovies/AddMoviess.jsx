da9051c6b201675974fdc8d34dca9301cfe1c482
Jastin-vlogger <justinkj765@gmail.com>
2022-12-10 23:10:38 +0530

    adding movie done

--- src/screens/admin/users/addmovies/AddMoviess.jsx
+++ src/screens/admin/users/addmovies/AddMoviess.jsx
@@ -1,17 +1,77 @@
-import React from 'react'
-import './addmovies.css'
+import React from "react";
+import "./addmovies.css";
 import { useForm } from "react-hook-form";
 import { useNavigate } from "react-router-dom";
+import { Input } from "@mui/icons-material";
+import { Button, Typography } from "@mui/material";
+import { useState } from "react";
+import useMutation from "../../../../hooks/useMutation";
+import axios from "../../../../axios/axios";
+
+const ErrorText = ({ children, ...props }) => (
+  <Typography sx={{ color: "error.main" }} {...props}>
+    {children}
+  </Typography>
+);
+
+const validFileTypes = ["image/jpg", "image/jpeg", "image/png"];
+const URL = "/api/admin/movieImage/upload";
 
 function AddMoviess() {
+  const [error, setError] = useState("");
+  const [isSubmit, setIsSubmit] = useState(false);
+
+  const {
+    mutate: uploadImage,
+    isLoading: uploading,
+    error: uploadError,
+  } = useMutation({ url: URL });
+
   const {
     register,
     handleSubmit,
     formState: { errors },
   } = useForm();
+
   const onSubmit = async (data) => {
-    console.log(data)
-  }
+    console.log(data);
+    const formData = new FormData();
+    formData.append("image", data.file[0]);
+    axios
+      .post("/api/admin/movieinfo", data)
+      .then(async(response) => {
+        console.log(response.data._id);
+        let id = response.data._id
+        await axios.post(`/api/admin/movieImage/upload/${id}`,formData,{
+          headers: {
+            "Content-Type": "multipart/form-data",
+          },
+        }).then(({data})=>{
+          console.log(data)
+        })
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
+
+  // const handleUpload = async (e,id) => {
+  //   if (!isSubmit) return;
+  //   else {
+  //     console.log("heelo im onchagne");
+  //     const file = e.target.files[0];
+  //     console.log(file);
+  //     if (!validFileTypes.find((type) => type === file.type)) {
+  //       setError("File must be in JPG/PNG format");
+  //       return;
+  //     }
+
+  //     const form = new FormData();
+  //     form.append("image", file);
+  //     await uploadImage(form);
+  //   }
+  // };
+
   return (
     <>
       <div className="wrapper rounded bg-white">
@@ -24,12 +84,12 @@ function AddMoviess() {
           <div className="form">
             <div className="row">
               <div className="col-md-6 mt-md-0 mt-3">
-                <label>Name</label>
+                <label>Title</label>
                 <input
                   type="text"
                   className="form-control"
-                  placeholder="Name"
-                  {...register("name", {
+                  placeholder="Title"
+                  {...register("title", {
                     required: true,
                     minLength: 4,
                     maxLength: 20,
@@ -52,11 +112,12 @@ function AddMoviess() {
                 </span>
               </div>
               <div className="col-md-6 mt-md-0 mt-3">
-                <label>Addess</label>
+                <label>Description</label>
                 <input
                   type="text"
                   className="form-control"
-                  {...register("address", {
+                  placeholder="Description"
+                  {...register("description", {
                     required: true,
                     minLength: 4,
                     maxLength: 50,
@@ -81,12 +142,12 @@ function AddMoviess() {
             </div>
             <div className="row">
               <div className="col-md-6 mt-md-0 mt-3">
-                <label>City</label>
+                <label>Genre</label>
                 <input
                   type="text"
                   className="form-control"
-                  placeholder="City"
-                  {...register("city", {
+                  placeholder="Genre"
+                  {...register("Genre", {
                     required: true,
                     minLength: 4,
                     maxLength: 20,
@@ -109,12 +170,12 @@ function AddMoviess() {
                 </span>
               </div>
               <div className="col-md-6 mt-md-0 mt-3">
-                <label>State</label>
+                <label>Director</label>
                 <input
                   type="text"
                   className="form-control"
-                  placeholder="State"
-                  {...register("state", {
+                  placeholder="Director"
+                  {...register("director", {
                     required: true,
                     minLength: 4,
                     maxLength: 20,
@@ -139,33 +200,28 @@ function AddMoviess() {
             </div>
             <div className="row">
               <div className="col-md-6 mt-md-0 mt-3">
-                <label>Email</label>
+                <label>Duration</label>
                 <input
-                  type="email"
+                  type="time"
                   className="form-control"
-                  placeholder="Email"
-                  {...register("email", {
+                  placeholder="Duration"
+                  {...register("Duration", {
                     required: true,
-                    pattern:
-                      /^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$/i,
                   })}
                 />
                 <p className="text-danger">
                   {errors.email?.type === "required" && (
-                    <span>Email is required</span>
-                  )}
-                  {errors.email?.type === "pattern" && (
-                    <span>Enter valied Email</span>
+                    <span>Duration is required</span>
                   )}
                 </p>
               </div>
               <div className="col-md-6 mt-md-0 mt-3">
-                <label>Phone No</label>
+                <label>Start date</label>
                 <input
-                  type="number"
+                  type="date"
                   className="form-control"
-                  placeholder="Phone No"
-                  {...register("phone", {
+                  placeholder="Start date"
+                  {...register("startDate", {
                     required: true,
                     minLength: 10,
                     maxLength: 10,
@@ -182,12 +238,62 @@ function AddMoviess() {
                   {errors.phone?.type === "maxLength" && (
                     <span>Phone Number must have 10 digits</span>
                   )}
-                  {/* {errors.name?.type === 'pattern' && (
+                </span>
+              </div>
+            </div>
+            <div className="row">
+              <div className="col-md-6 mt-md-0 mt-3">
+                <input
+                  type="file"
+                  className="form-control"
+                  {...register("file")}
+                />
+                <span className="text-danger"></span>
+                {/* <input
+                  id="imageInput"
+                  type="file"
+                  hidden
+                  onChange={handleUpload}
+                />
+                <Button
+                  as="label"
+                  htmlFor="imageInput"
+                  colorScheme="blue"
+                  variant="contained"
+                  cursor="pointer"
+                  isLoading={uploading}
+                >
+                  Upload
+                </Button> */}
+                {error && <ErrorText>{error}</ErrorText>}
+                {uploadError && <ErrorText>{uploadError}</ErrorText>}
+              </div>
+              <div className="col-md-6 mt-md-0 mt-3">
+                <label>youtube link</label>
+                <input
+                  type="text"
+                  className="form-control"
+                  placeholder="Youtube trailer Link"
+                  {...register("YoutubeLink", {
+                    required: true,
+                    minLength: 4,
+                    maxLength: 50,
+                    pattern: /^[^\s]+(?:$|.*[^\s]+$)/,
+                  })}
+                />
+                <span className="text-danger">
+                  {errors.address?.type === "required" && (
+                    <span>address is required</span>
+                  )}
+                  {errors.address?.type === "minLength" && (
+                    <span>address must morethan or equal to 4 Character</span>
+                  )}
+                  {errors.address?.type === "maxLength" && (
+                    <span>address must less than 50 Character</span>
+                  )}
+                  {errors.address?.type === "pattern" && (
                     <span>Should not have spaces</span>
-                  )} */}
-                  {/* {errors.phone?.type === "valueAsNumber" && (
-                        <span>Enter Only Number</span>
-                      )} */}
+                  )}
                 </span>
               </div>
             </div>
@@ -200,7 +306,7 @@ function AddMoviess() {
         </form>
       </div>
     </>
-  )
+  );
 }
 
-export default AddMoviess
+export default AddMoviess;
